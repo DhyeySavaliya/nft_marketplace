e2d8a4fffedf0dd61f70fef037fb9d6d138d9a6c
Dhyey0712 <dhyeysavaliya75@gmail.com>
2023-04-15 11:38:04 +0530

    Completed the app and pushing the remaining code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:nft/screens/home.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nft/screens/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'NFT App',
       debugShowCheckedModeBanner: false,
-      title: "My NFTs",
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: const HomeScreen(),
+      theme: ThemeData(
+        fontFamily: 'Dsignes',
+        primarySwatch: Colors.blue,
+      ),
+      home: const OnBoarding(),
     );
   }
 }
